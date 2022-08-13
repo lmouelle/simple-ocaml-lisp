de3a74fcd1667f1c66d38cc77813d227b159042e
fake@email.com <fake@email.com>
2022-08-13 11:45:10 -0700

    implement parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,17 +7,25 @@ open Angstrom
   Symbol: \D+
 *)
 
+type sexp = Number of int | Symbol of string | List of sexp list
+
 let is_number_char = function '0' .. '9' -> true | _ -> false
 
-let symbol = take_while1 (fun c -> is_number_char c |> not && c <> ')' && c <> '(') >>= fun s -> return @@ `Symbol s
+let is_seperating_whitespace = function ' ' | '\t' -> true | _ -> false
 
-let number = take_while1 is_number_char >>= fun s -> return @@ `Number (int_of_string s)
+let is_symbol_char = function ')' | '(' | ' ' | '\t' | ';' -> false | _ -> true
 
-let atom = symbol <|> number
+let whitespace_dropping_parser = skip_while is_seperating_whitespace
+
+let symbol = take_while1 is_symbol_char >>= fun s -> return @@ Symbol s 
+
+let number = take_while1 is_number_char >>= fun s -> return @@ Number (int_of_string s)
+
+let atom = number <|> symbol
 
 let sexp = fix (fun sexp ->
-    let list = char '(' *> many sexp <* char ')' >>= fun l -> return @@ `List l in
-    list <|> atom
+    let list = char '(' *> many sexp <* char ')' >>= fun l -> return @@ List l in
+    whitespace_dropping_parser *> (list <|> atom) <* whitespace_dropping_parser
   )
 
 let parse_with str parser = parse_string ~consume:All parser str
