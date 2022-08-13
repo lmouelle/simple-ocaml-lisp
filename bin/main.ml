b1c2e904d40ce8e9e264cd02f23bfb927b0f14e3
fake@email.com <fake@email.com>
2022-08-13 14:06:07 -0700

    Add basic repl func entrypoint

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,20 @@
 open Lib.Parser
+open Lib.Eval
 
+let rec repl env = 
+  print_string "> ";
+  let input = read_line () in
+  match parse input with
+  | Error msg -> Printf.eprintf "Error parsing string: %s" msg
+  | Ok sexp ->
+    (
+      let result, env' = eval sexp env in
+      let result_string = sexp_to_string result in
+      print_string result_string;
+      print_newline ();
+      repl env';
+    )
+  
+    
 let () =
-  print_endline "Hello, world!"
+  repl [] ;;   
