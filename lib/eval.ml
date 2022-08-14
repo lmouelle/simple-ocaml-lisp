84db98cf8435353d419b508cc5357d832a374d5b
fake@email.com <fake@email.com>
2022-08-13 23:59:23 -0700

    Add define, quote and env primatives

--- lib/eval.ml
+++ lib/eval.ml
@@ -1,21 +1,8 @@
 open Parser
 
-let rec eval sexp env =
-  match sexp with
-  | Number n -> (Number n, env)
-  | Symbol s ->
-    let subsexp = List.assoc s env in
-    eval subsexp env
-  | Boolean b -> (Boolean b, env)
-  | List (Symbol "if" :: cond :: iftrue :: iffalse :: []) -> 
-      let condval, _  = eval cond env in
-      let resultval = match condval with
-      | Boolean true -> iftrue
-      | Boolean false -> iffalse
-      | _ -> failwith "(if bool exp1 exp2)"
-      in
-      eval resultval env
-  | _ ->  (sexp, env)
+exception NoSuchVariable of string
+
+type environment = (string * sexp) list
 
 let rec sexp_to_string = function
 | Number n -> string_of_int n;
@@ -29,3 +16,30 @@ let rec sexp_to_string = function
   in
   let strings = List.map sexp_to_string l in
   "(" ^ listjoin strings ^ ")"
+
+let rec eval sexp (env : environment) =
+  match sexp with
+  | Number n -> (Number n, env)
+  | Symbol s ->
+    (try 
+      let subsexp = List.assoc s env in
+      eval subsexp env
+    with Not_found -> raise @@ NoSuchVariable s)
+  | Boolean b -> (Boolean b, env)
+  | List (Symbol "if" :: cond :: iftrue :: iffalse :: []) -> 
+      let condval, _  = eval cond env in
+      let resultval = match condval with
+      | Boolean true -> iftrue
+      | Boolean false -> iffalse
+      | _ -> failwith "(if bool exp1 exp2)"
+      in
+      eval resultval env
+  | List (Symbol "define" :: Symbol symbol :: expression :: []) ->
+    let symbol_value, _ = eval expression env in
+    let env' = (symbol, symbol_value) :: env in
+    symbol_value, env'
+  | List (Symbol "quote" :: expr :: []) -> Symbol (sexp_to_string expr), env
+  | List (Symbol "env" :: []) ->
+    let f = List.map (fun (name, value) -> List [Symbol name; value]) env in
+    (List f, env)
+  | _ ->  (sexp, env)
