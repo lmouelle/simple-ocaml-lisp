3349003bbdac692276844864f83e74536880f31d
fake@email.com <fake@email.com>
2022-08-16 12:52:10 -0700

    Create AST in parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -12,7 +12,23 @@ type procedure = {
   body : sexp list -> sexp
 }
 
-and sexp = Number of int | Symbol of string | Boolean of bool | Procedure of procedure | List of sexp list
+and sexp =  (* Lexer portion *)
+| Number of int 
+| Symbol of string 
+| Boolean of bool 
+| Procedure of procedure 
+| List of sexp list
+and exp = (* Parser portion *)
+| Literal of sexp
+| Variable of string
+| If of exp * exp * exp
+| And of exp * exp
+| Or of exp * exp
+| Call of exp * exp list
+| Definition of def
+and def =
+| Value of string * exp
+| Expression of exp
 
 let is_number_char = function '0' .. '9' -> true | _ -> false
 
@@ -41,3 +57,22 @@ let sexp = fix (fun sexp ->
 let parse_with str parser = parse_string ~consume:All parser str
 
 let parse s = parse_with s sexp
+
+
+exception AstError of string
+let rec built_ast sexp = 
+  match sexp with
+  | Procedure _ -> raise @@ AstError "Cannot build ast from procedure"
+  | Number _ | Boolean _ -> Literal sexp
+  | Symbol s -> Variable s
+  | List [Symbol "if"; cond; iftrue; iffalse] ->
+    If (built_ast cond, built_ast iftrue, built_ast iffalse)
+  | List [Symbol "and"; exp1; exp2] ->
+    And (built_ast exp1, built_ast exp2)
+  | List [Symbol "or"; exp1; exp2] ->
+    Or (built_ast exp1, built_ast exp2)
+  | List [Symbol "define"; Symbol s; e] ->
+    Definition (Value (s, built_ast e))
+  | List (fn :: args) ->
+    Call (built_ast fn, List.map built_ast args)
+  | List _ -> Literal sexp
